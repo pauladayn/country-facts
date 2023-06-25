3b5900c4a04a7899421b9355ef6159dfb5d7a53b
Paula <pnieto@portfoliopersonal.com>
2023-06-25 18:08:22 -0300

    fix: final changes

--- app/components/molecules/Card.tsx
+++ app/components/molecules/Card.tsx
@@ -9,11 +9,10 @@ import { transformApiData } from '@/app/utils/transformApiInfo';
 export const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'] });
 
 export const Card = ({ title, subtitle, language, currencies, timezone, image, link }: CardType) => {
-    const getLanguages = language && Object.keys(language).map(lang => language[lang as keyof typeof language])
-    const getCurrency = currencies && Object.keys(currencies).map(currency => ({ values: currencies[currency as keyof typeof currencies], currency }))
-    console.log('get currency', getCurrency && getCurrency[0].values);
 
-    console.log('xxxxx------->', transformApiData(language))
+    const getCurrency = currencies && Object.keys(currencies).map(currency => ({ values: currencies[currency as keyof typeof currencies], currency }))
+
+    const languageInfo = language && transformApiData(language)
 
     return (
         <div className='container pointer-events-auto max-w-xs transition-all hover:scale-105'>
@@ -29,14 +28,18 @@ export const Card = ({ title, subtitle, language, currencies, timezone, image, l
                     }
                     <CardTitle title={title} subtitle={subtitle} />
                     <div className={`grid place-items-center gap-3 ${poppins.className}`}>
-                        <p className='text-xs'>{getLanguages?.length > 1 ? 'Main Languages' : 'Main language'}</p>
+                        <p className='text-xs'>{languageInfo && languageInfo?.length > 1 ? 'Main Languages' : 'Main language'}</p>
                         <div className='min-w-full cursor-pointer rounded-full   bg-white dark:bg-transparent px-3 py-2 text-center hover:bg-transparent hover:text-white hover:ring-1 hover:ring-white dark:ring-teal-300 dark:ring-1 dark:hover:bg-white dark:hover:text-purple-500'>
-                            {getLanguages?.length > 1 ? (
-                                <div className='flex justify-around items-center'>{
-                                    getLanguages.slice(0, 3).map((lang, index) => (<p className='full-rounded text-sm' key={index}>{lang}</p>))}</div>) : (<p className='text-sm'>{getLanguages?.toString() || getLanguages}</p>)}
+                            {languageInfo && languageInfo?.length > 1 ? (
+                                <div className='flex justify-around items-center'>
+                                    {
+                                        languageInfo.slice(0, 3).map((lang, index) => (<p className='full-rounded text-sm' key={index}>{lang}</p>))}</div>)
+                                :
+                                (<p className='text-sm'>{languageInfo?.toString() || languageInfo || '---'}</p>)}
                         </div>
                         <CardDescriptionItem title='Currencies'>
-                            {`${getCurrency && getCurrency[0].currency} ${getCurrency && getCurrency[0].values?.name} - ${getCurrency && getCurrency[0].values?.symbol}`}
+
+                            {`${getCurrency ? getCurrency[0].currency : '-'} ${getCurrency ? getCurrency[0].values['name'] : '-'} - ${getCurrency ? getCurrency[0].values['symbol'] : ''}`}
                         </CardDescriptionItem>
                         <CardDescriptionItem title='Timezone'>{timezone.slice(0, 3)}</CardDescriptionItem>
                     </div>
@@ -50,15 +53,6 @@ export const Card = ({ title, subtitle, language, currencies, timezone, image, l
                                 </svg>
                             </Link>
                         )}
-                        <button className='text-white'>
-                            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='h-6 w-6'>
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    d='M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                                />
-                            </svg>
-                        </button>
                     </div>
                 </div>
             </div>
